ccf5ca5bb8db9d736692d31e23b7a7fb0a411034
iamkg0 <chonpsthetent@gmail.com>
2025-04-23 17:35:15 +0300

    started flexible object to gather logs

--- Prototype/tools.py
+++ Prototype/tools.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import os
+import csv
+import pandas as pd
 '''
 Sample random inputs
 Stores in patterns (dict)
@@ -23,3 +25,43 @@ def sampler(num_inputs=10, num_rand_patterns=2):
                     patterns[p] = seq
                     break
     return patterns, num_inputs, num_patterns
+
+
+
+'''
+log handler
+'''
+
+class Log_handler:
+    def __init__(self, path, filename):
+        self.map = {}
+        self.data = []
+        self.path = str(path)
+        self.filename = filename
+
+    def create(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        if os.path.isfile(self.path+self.filename):
+            print('This file already exists')
+        else:
+            with open(self.path + '\\' + self.filename, 'w') as file:
+                writer = csv.writer(file, delimiter='\t',lineterminator='\n')
+        
+    def define_cols(self, cols):
+        with open(self.path + '\\' + self.filename, 'w') as file:
+            writer = csv.writer(file, delimiter='\t',lineterminator='\n')
+            self.data = [[] for i in range(len(cols))]
+            for i in range(len(cols)):
+                self.map[cols[i]] = self.data[i]
+            writer.writerow(cols)
+
+    def append_sample(self, sample):
+        for i in range(len(sample)):
+            self.data[i].append(sample[i])
+        
+    def write_sample(self, sample):
+        with open(self.path + '\\' + self.filename, 'a') as file:
+            writer = csv.writer(file, delimiter='\t',lineterminator='\n')
+            writer.writerow(sample)
+                
